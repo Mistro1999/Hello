78bfd01df9ab8aee1dce2f99a9dbc504a5689125
Mistro1999 <42555944+Mistro1999@users.noreply.github.com>
2019-01-17 11:35:33 -0600

    Added Hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Amur Al harthi
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Amur Al harthi!\n");
+
+  return 0;
+}
